ac2f4264f6f68a601aa96169147a3d14788f5f01
lucas <lucas.r.s.s.2031@gmail.com>
2026-07-13 20:13:45 -0300

    feat: criacao da classe livro

--- Program.cs
+++ Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿List<Livros> livros = new();
+livros.Add(new Livros
+{
+    Nome = "pika",
+    Preco = 12.00,
+    Alugel = 13,
+    Codigo = 1,
+    quantidade = 5,
+});
+
+Console.WriteLine("");
+foreach (Livros livro  in livros)
+{
+    Console.WriteLine(livro.Nome);
+}
