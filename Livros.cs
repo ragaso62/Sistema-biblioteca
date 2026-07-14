e000daf2070dd72b4a4a80123c52e5ff8624f135
lucas <lucas.r.s.s.2031@gmail.com>
2026-07-13 21:45:44 -0300

    feat: criando interface via terminal

--- Livros.cs
+++ Livros.cs
@@ -2,19 +2,24 @@
 {
     private double _preco;
     private double _alugel;
+    private int _quantidade;
     public string Nome {  get; set; }
     public int Codigo { get; set; }
-    public int quantidade
+    public int Quantidade
     {
         get
         {
-            return quantidade;
+            return _quantidade;
         }
         set
         {
             if(value <= 0)
             {
                 Console.WriteLine("Valor negativo não aceitado");
+            }
+            else
+            {
+                _quantidade = value;
             }
         }
     }
@@ -29,6 +34,10 @@
             if (value <= 0 || value > 10000)
             {
                 Console.WriteLine("o valor escrito é maior que 10000 ou menor que 0, por favor insira um valor valido");
+            }
+            else
+            {
+               _preco = value;
             }
         }
     }
@@ -48,12 +57,15 @@
             {
                 Console.WriteLine("O valor do aluguel esta muito auto, por favor insira um valor abaixo de 100");
             }
+            else
+            {
+                _alugel = value;
+            }
         }
     }
     public void descontoLivro(int valor)
     {
-       double desconto = Preco * valor / 100;
-        Preco -= desconto;
+        Preco = Preco - (Preco * valor / 100.0);
     }
     public void aumentarPreco(int valor)
     {
@@ -66,6 +78,6 @@
 
     public void AlterarEstoque(int valor)
     {
-        quantidade = valor;
+        Quantidade = valor;
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿List<Livros> livros = new();
+﻿int escolha = 0;
+
+List<Livros> livros = new();
 livros.Add(new Livros
 {
     Nome = "pika",
@@ -7,9 +9,38 @@ livros.Add(new Livros
     Codigo = 1,
     quantidade = 5,
 });
-
-Console.WriteLine("");
-foreach (Livros livro  in livros)
+livros.Add(new Livros
 {
-    Console.WriteLine(livro.Nome);
+    Nome = "pika2"
+});
+
+Console.WriteLine("=== BEM VINDO AO SISTEMA BIBLIOTECA ===");
+Console.WriteLine("o que deseja fazer hoje?\n1 - aumentar desconto\n2 - aumentar preço de um livro\n3 - diminuir preço de um livro\n4 - alterar estoque\n5 -listar livros\n 0 - sair");
+escolha = int.Parse(Console.ReadLine());
+
+if(escolha == 0)
+{
+    
+}
+else if (escolha == 1)
+{
+    Console.WriteLine("qual livro deseja dar desconto?");
+    foreach (Livros livro  in livros)
+    {
+        Console.WriteLine($"{livro.Codigo} - {livro.Nome}");
+    }
+    int escolha2 = int.Parse(Console.ReadLine());
+    Livros encontrar = livros.Find(l => l.Codigo == escolha2);
+    if (encontrar != null)
+    {
+        Console.WriteLine($"livro encontrado {encontrar.Nome}");
+        Console.WriteLine("qual desconto deseja aplicar?");
+        int desconto = int.Parse(Console.ReadLine());
+        encontrar.descontoLivro(desconto);
+        Console.WriteLine($"O livro agora tem {desconto}% de desconto e estar com o valor R${encontrar.Preco}");
+    }
+    else
+    {
+        Console.WriteLine("livro não encontrado");
+    }
 }
